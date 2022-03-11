2378bb1fb4fe551fb72cd826a7327c1a389c5343
pexure <pexure@gmail.com>
2022-03-11 03:53:46 +0000

    m

--- dev/make_minitrain_info.py
+++ dev/make_minitrain_info.py
@@ -4,11 +4,11 @@ import os
 
 def main():
     info_path = '/data/common/waymo/infos_train_01sweeps_filter_zero_gt.pkl'
-    save_path = 'data/common/waymo/infos_train1000_01sweeps_filter_zero_gt.pkl'
+    save_path = '/data/common/waymo/infos_train1000_01sweeps_filter_zero_gt.pkl'
     info = pickle.load(open(info_path, 'rb'))
     mini_info = info[:1000]
     assert not os.path.isfile(save_path)
-    pickle.dump(mini_info, open(save_path, 'w'))
+    pickle.dump(mini_info, open(save_path, 'wb'))
 
 
 main()
